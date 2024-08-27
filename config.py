d01065c76c3d7cd420e8effe14f4e901e54b62cb
anandadagit <anand@ada.ac.uk>
2024-08-27 21:27:26 +0100

    added environment variable

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
 
 # When deploying, set in the environment to the PostgreSQL URL
 #connection_string = 'postgresql://postgres:postgres@localhost:5432/exercises'
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://anand:xwDGjtTBf8d1LF62diyINDilAQmKR54t@dpg-cr733ji3esus73dn8gr0-a.oregon-postgres.render.com/test_n5z8')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
